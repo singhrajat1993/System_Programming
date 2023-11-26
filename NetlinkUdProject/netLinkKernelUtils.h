dcc3df4572406182921935451427e157573b46e5
Rajat Singh <singh.rajat.dec93@gmail.com>
2023-11-26 12:53:56 +0530

    Adding Netlink Files

---
+++ NetlinkUdProject/netLinkKernelUtils.h
@@ -0,0 +1,133 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  common.h
+ *
+ *    Description:  This file contains common routines and definitions to be used in kernel and user space
+ *
+ *        Version:  1.0
+ *        Created:  11/22/2019 12:16:25 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Er. Abhishek Sagar, Juniper Networks (https://csepracticals.wixsite.com/csepracticals), sachinites@gmail.com
+ *        Company:  Juniper Networks
+ *
+ *        This file is part of the Netlink Sockets Course distribution (https://github.com/sachinites) 
+ *        Copyright (c) 2019 Abhishek Sagar.
+ *        This program is free software: you can redistribute it and/or modify it under the terms of the GNU General 
+ *        Public License as published by the Free Software Foundation, version 3.
+ *        
+ *        This program is distributed in the hope that it will be useful, but
+ *        WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *        General Public License for more details.
+ *
+ *        visit website : https://csepracticals.wixsite.com/csepracticals for more courses and projects
+ *                                  
+ * =====================================================================================
+ */
+
+#ifndef __NL_COMMON__
+#define __NL_COMMON__
+
+#include <linux/netlink.h>
+
+/* maximum payload size in Bytes exchanged between kernel and userspace
+ * in either directions*/
+#define MAX_PAYLOAD     1024
+#define RT_NAME_LEN     32
+#define TLV_OVERHEAD    (RTA_ALIGN(sizeof(struct rtattr)))
+
+
+static inline int 
+TLV_ADD(char *tlv_start, int type, int len, char *val)
+{
+    
+    struct rtattr *tlv_start_ptr = (struct rtattr *)tlv_start;
+    tlv_start_ptr->rta_len = RTA_ALIGN(len);
+    tlv_start_ptr->rta_type = (__u16)type;
+    memset(RTA_DATA(tlv_start_ptr), 0, RTA_ALIGN(len));
+    memcpy(RTA_DATA(tlv_start_ptr), val, len);
+    return TLV_OVERHEAD + RTA_ALIGN(len); /*Alternatively : return RTA_SPACE(len)*/
+}
+
+/*User defined NL MSG TYPES, should be > 16 */
+#define NLMSG_GREET     20
+#define NLMSG_RT_NEW_CREATE   21
+
+
+/*TLVs Code Points*/
+#define NETLINK_TLV_RT_CREATE   1
+
+
+
+/* user define netlink protocol.
+ * It should match with Netlink Socket protocol opened in kernel space.*/
+
+/* Define a New Netlink protocol ID
+ * In file linux/netlink.h, you can see in line no [8-33]
+ * lists the reserved IDs used for Netlink protocols already implemented
+ * in Linux. We can use upto max 31th protocol number for Netlink. Let
+ * us just pick the unused one - 31 below */
+
+#define NETLINK_TEST_PROTOCOL   31
+
+static inline char *
+netlink_get_msg_type(__u16 nlmsg_type){
+
+    switch(nlmsg_type){
+        case NLMSG_NOOP:
+            return "NLMSG_NOOP";
+        case NLMSG_ERROR:
+            return "NLMSG_ERROR";
+        case NLMSG_DONE:
+            return "NLMSG_DONE";
+        case NLMSG_OVERRUN:
+            return "NLMSG_OVERRUN";
+        case NLMSG_GREET:
+            return "NLMSG_GREET";
+        case NLMSG_RT_NEW_CREATE:
+            return "NLMSG_RT_NEW_CREATE";
+        default:
+            return "NLMSG_UNKNOWN";
+    }
+}
+
+static inline void
+nlmsg_dump(struct nlmsghdr *nlh){
+#ifdef __KERNEL_CODE__
+    printk(KERN_INFO "Dumping Netlink Msgs Hdr");
+#else
+    printf("Dumping Netlink Msgs Hdr");
+#endif
+#ifdef __KERNEL_CODE__
+    printk(KERN_INFO "  Netlink Msg Type = %s", 
+        netlink_get_msg_type(nlh->nlmsg_type));
+#else
+    printf("  Netlink Msg Type = %s",
+        netlink_get_msg_type(nlh->nlmsg_type));
+#endif
+#ifdef __KERNEL_CODE__
+    printk(KERN_INFO "  Netlink Msg len  = %d", nlh->nlmsg_len);
+#else
+    printf("  Netlink Msg len  = %d", nlh->nlmsg_len);
+#endif
+#ifdef __KERNEL_CODE__
+    printk(KERN_INFO "  Netlink Msg flags  = %d", nlh->nlmsg_flags);
+#else
+    printf("  Netlink Msg flags  = %d", nlh->nlmsg_flags);
+#endif
+#ifdef __KERNEL_CODE__
+    printk(KERN_INFO "  Netlink Msg Seq#  = %d", nlh->nlmsg_seq);
+#else
+    printf("  Netlink Msg Seq#  = %d", nlh->nlmsg_seq);
+#endif
+#ifdef __KERNEL_CODE__
+    printk(KERN_INFO "  Netlink Msg Pid#  = %d", nlh->nlmsg_pid);
+#else
+    printf("  Netlink Msg Pid#  = %d", nlh->nlmsg_pid);
+#endif
+}
+
+#endif /* __NL_COMMON__ */
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # System_Programming
 It's just personal purpose only to learn and store my learning and Practice source codes.
+To compile source code of linux kernel module we need to use command 'make' simply
+To Check logs of kernel modules from the 'kern.log' file only cmd- 'tail -f /var/log/kern.log'
+To insert module (file .ko) run command sudo insmod <file.ko>
+
+
+Inserting a Module(Driver) 'sudo insmod file.ko'
+removing a Module 'sudo rmmod file'
+to check modules got inserted 'lsmod'
+
+
+*************************************************************************************************
+
+============Netlink Sokets===========
+
+*	Netlink Sockets are especially designed to facilitate clean bidirectional communication between users and kernal space.
+*	Other Techniques also can be used for US<>KS communication, but those technique were invented for the perpose. like Ioctls, device files, System calls.
+*	But a Soket based technique was developed to build the unified interface using wich user space application (USA) can interact with various kernel subsystem.
+
+	int skt_fd = socket(socket address Family, comunication type , protocol ID);
+
+Project Details:
+Communication between user space application and Routing Table Mgr (Kernal Subsystem)
+
+USA(User Space Apllication) Action:
+Perform CRUD operations.
+
+KS(Kernel Space) Actions:
+*	Write a Linux kernal Module which will behave as Routing Table MGr residing in kernel space.
+*	It shall process CRUD orders coming from application.
+
+Idea: To explore Netlink Socket based communication between US & KS by developing USA which interact with our LKM which is in-charge of our Routing Subsystem of Kerneli.
+
+
+Netlink Messege format:
+
+Netlink Msg Hadder(16B) Padding(fewBytes) Payload(msg)
+
+struct nlnmsghdr{
+		  u32 nlmsg_len;
+		  u16 nlmsg_type;
+		  u16 nlmsg_flags;
+                  u32 nlmsg_seg;
+                  u32 nlmsg_pid;
+                }
+
